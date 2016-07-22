ac326ceb95089b74d22190d15b9e431cd48f8f17
Wes Young <wes.young14@gmail.com>
2016-07-21 19:38:33 -0600

    Dry Run Edited

--- TechDryRun.js
+++ TechDryRun.js
@@ -1,9 +1,45 @@
 //Tech Dry Run
 
-var hrr18 = {
-  name: 'Keeley Nakamoto'
-  name2: 'Wes Young'
-}
+//Tech Dry Run
 
+var hrr18 = {
+  name: 'Keeley Nakamoto',
+  anothername: 'Jim',
+  jeffName: 'Jeff Bernstein',
+  yetAnother: 'Dan jrrn eerr n nnrr rner rnnr  eees n oynllolonyro ynronllooo loo
+
+ ro
+nnr
+
+ name: 'Caleb',
+
+  neekon: neekon,
+  per
+
+so
+nskf;lasdkf;lasdkf
+
+  na
+myyyydddd
+
+yy
+PayPe
+
+// STEPHENIE CHANGES -- // 
+
+y: 'PPayt:yy}:::
+:
+yylo
+ama
+eeeaeema:meeaamaeaaeeaeaaeeeeeeee:e::eee emmea::a esnY
+
+
+Hello
+
+
+
+sa
+name: 'Mchael Olorunnisola'
+}
 
 
